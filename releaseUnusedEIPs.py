310ef869df4e9e657f5f09cb0e586ec446b604e0
Ravi Petlur <ravi.petlur@gmail.com>
2020-04-25 00:58:27 +0530

    Script - Added listing RDS instances for a region Bug - Fixed logging (Still not generic)

--- releaseUnusedEIPs.py
+++ releaseUnusedEIPs.py
@@ -1,18 +1,27 @@
 import boto3
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+
 def elastic_ips_cleanup(region):
     client = boto3.client('ec2', region_name=region)
     addresses_dict = client.describe_addresses()
     for eip_dict in addresses_dict['Addresses']:
         if "InstanceId" not in eip_dict:
-            print(eip_dict['PublicIp'] +
-                  " doesn't have any instances associated, releasing")
+            logging.info("{eip_dict['PublicIp']} doesn't have any instances associated, releasing")
             client.release_address(AllocationId=eip_dict['AllocationId'])
 
 
 regions = [r['RegionName'] for r in boto3.client('ec2').describe_regions()['Regions']]
 
 for region in regions:
-    logging.info(f"Cleaning {region}")
+    logging.info("Cleaning {region}")
     elastic_ips_cleanup(region)
